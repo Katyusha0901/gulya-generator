d6dfd79e8e5a34d607409c838f9e0831a13aa18f
Ekaterina Bondareva <1978231@bk.ru>
2024-02-23 15:44:36 +0600

    css

--- src/Explanation.tsx
+++ src/Explanation.tsx
@@ -8,7 +8,9 @@ export const Explanation: React.FC = () => {
       {isOpen && (
         <div className="explanation">
           <div className="explanation__top">
-            <div className="explanation__title"></div>
+            <div className="explanation__title">
+              Поймите свои эмоции и повысьте эмоциональный интеллект
+            </div>
             <div
               className="explanation__close"
               onClick={() => setIsOpen(false)}
@@ -16,7 +18,10 @@ export const Explanation: React.FC = () => {
               ×
             </div>
           </div>
-          <div className="explanation__text"></div>
+          <div className="explanation__text">
+            Откройте карточки 8 эмоций и расположите их в правильном порядке от
+            "Грусти" до "Счастья" с помощью стрелок.
+          </div>
         </div>
       )}
     </>
